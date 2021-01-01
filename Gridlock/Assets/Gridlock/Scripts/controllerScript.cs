13bd1431b2d38639de8409ea8bb39194d35452ac
Lo-oker <42920029+Unibeat-Games@users.noreply.github.com>
2021-01-01 19:29:38 +0530

    second commit

--- Gridlock/Assets/Gridlock/Scripts/controllerScript.cs
+++ Gridlock/Assets/Gridlock/Scripts/controllerScript.cs
@@ -11,6 +11,9 @@ public class controllerScript : MonoBehaviour
     [SerializeField] public LeanTweenType Type;
     [SerializeField] public GameObject from;
     [SerializeField] public GameObject to;
+    [SerializeField] public bool ScreeTransition = false;
+    
+    [SerializeField] public LeanTweenType screeTransitionType;
 
     
 
@@ -20,15 +23,24 @@ public class controllerScript : MonoBehaviour
     public void buttonPressed()
     {
         LeanTween.scale(gameObject, new Vector3(1.2f, 1.2f, 1f), 0.05f).setEase(Type);
-        LeanTween.scale(gameObject, new Vector3(1f, 1f, 1f), 0.05f).setEase(Type).setDelay(0.05f);
-        LeanTween.delayedCall(gameObject, 0.1f, ()=>
-        {
-            transition();
-            if (to.name == "Levels Menu")
+            LeanTween.scale(gameObject, new Vector3(1f, 1f, 1f), 0.05f).setEase(Type).setDelay(0.05f);
+            LeanTween.delayedCall(gameObject, 0.1f, ()=>
             {
-                _uiManagerScript.LevelsMenuFunction();
-            }
-        });
+                if (ScreeTransition)
+                {
+                    Screen_transition();
+                }
+                else
+                {
+                    transition();
+                }
+                if (to.name == "Levels Menu")
+                {
+                    _uiManagerScript.LevelsMenuFunction();
+                }
+            });
+        
+        
     }
 
     
@@ -49,6 +61,20 @@ public class controllerScript : MonoBehaviour
     {
         from.SetActive(false);
         to.SetActive(true);
+    }
+    public void Screen_transition()
+    {
+        LeanTween.scale(@from, new Vector3(1.1f, 1.1f, 1.1f), 0.05f).setEase(screeTransitionType);
+        LeanTween.scale(@from, new Vector3(1f, 1f, 1f), 0.05f).setEase(screeTransitionType).setDelay(0.05f);
+        to.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        LeanTween.delayedCall(gameObject, 0.1f, ()=>
+        {
+            
+            to.SetActive(true);
+            from.SetActive(false);
+            LeanTween.scale(@to, new Vector3(1f, 1f, 1f), 0.05f).setEase(screeTransitionType);
+           
+        });
     }
    
     
